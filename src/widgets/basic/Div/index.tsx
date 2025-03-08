170def46cba2e3906a52a913ae36f193c570fc82
tyd63 <june3dylan@gmail.com>
2025-03-09 00:11:59 +0800

    feat: 支持div组件输入内容

--- src/widgets/basic/Div/index.tsx
+++ src/widgets/basic/Div/index.tsx
@@ -3,6 +3,7 @@ import { defineComponent } from '@/widgets/utils/define'
 import { genStyle } from '@/widgets/utils/style'
 import { renderPreview } from '@/widgets/utils/render'
 import { renderSlot } from '@/renderer/renderSlot'
+import { createInputProp } from '@/widgets/utils/props'
 
 const registerConfig = () => {
   return defineComponent({
@@ -11,7 +12,13 @@ const registerConfig = () => {
     group: 'basic',
     draggable: true,
     icon: () => <ContainerIcon />,
-    props: {},
+    props: {
+      content: createInputProp({
+        label: '内容',
+        value: '',
+        clearable: true
+      })
+    },
     model: {},
     style: genStyle(),
     events: [],
@@ -20,9 +27,15 @@ const registerConfig = () => {
       return renderPreview({ icon: component.icon, name: component.name })
     },
     render: ({ props, style, children, id }) => {
+      const renderContent = () => {
+        if (props.content) {
+          return props.content
+        }
+        return renderSlot(id, 'default', children)
+      }
       return (
         <div {...props} style={style}>
-          {renderSlot(id, 'default', children)}
+          {renderContent()}
         </div>
       )
     }
